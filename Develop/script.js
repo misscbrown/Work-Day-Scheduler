4f567a11d63dc7fded68480865a9dffbaa2fb450
cee breezy <djmisscbrown@hotmail.com>
2022-01-21 14:31:24 +0000

    fixed bug with moments/added items to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,53 @@
+$(".saveBtn").on("click", function () {
+  var timeSlotActivity = $(this).siblings(".description").val();
+  console.log(timeSlotActivity);
+  var timeSlotId = $(this).parent().attr("id");
+  console.log(timeSlotId);
 
-// sets current date and time
-var todayDate = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").html(todayDate);
-
-
-
-// var schedule = document.getElementById("col-10 description")
-// var saveBtn = document.getElementById("saveBtn col-1")
-
-// saveBtn.addEventListener("click", function(event) {
-//     event.preventDefault();
-
-//     var comment = {
-//         text: schedule.value(),
-//     }
-
-//     localStorage.setItem("comment", JSON.stringify(comment));
-// });
-
-
-// saveBtn.addEventListener("click", function(event) {
-//     var textInput = {
-//         text: schedule.value(),
-
-//     var textInputstr = JSON.stringify(textInput);
-
-//     localStorage.setItem("textInput", textInputstr)
-//     }
-
-// }
-
+  localStorage.setItem(timeSlotId, timeSlotActivity);
+});
 //Saves input text to local storage - when page refreshes, text is still saved
-
 function saveText() {
-    
-    var inputText = document.querySelector("#textarea");
-    var output = document.querySelector("#textarea");
-    var saveButton = document.querySelector("#saveBtn");
-  
-    saveButton.addEventListener("click", updateOutput);
-  
-    output.textContent = localStorage.getItem("content");
-    inputText.value = localStorage.getItem("content");
-  
-    function updateOutput() {
-      localStorage.setItem("content", inputText.value);
-  
-      output.textContent = inputText.value;
-    }
+  var inputText = document.querySelector("#textarea");
+  var output = document.querySelector("#textarea");
+  var saveButton = document.querySelector("#saveBtn");
+
+  saveButton.addEventListener("click", updateOutput);
+
+  output.textContent = localStorage.getItem("content");
+  // inputText.value = localStorage.getItem("content");
+
+  function updateOutput() {
+    localStorage.setItem("content", inputText.value);
+
+    output.textcontent = localstorage.getitem("content");
+
+    //   output.textContent = inputText.value;
   }
+}
+
+//set current day and time at top of screen
+var currentDate = moment().format("dddd MMM Do YYYY, h:mm a ");
+$("#currentDay").append(currentDate);
+
+// function for changing colour according to past, present and future
+function currentTime() {
+  var currentHour = moment().hours();
+  console.log("Current Hour", currentHour);
+  $(".time-block").each(function () {
+    console.log(this);
+    var plannerTime = parseInt($(this).attr("id"));
+
+    if (plannerTime < currentHour) {
+      $(this).addClass("past");
+    } else if (plannerTime === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
+
+var timeCheckInterval = setInterval(currentTime, 20000);
+
+currentTime();
